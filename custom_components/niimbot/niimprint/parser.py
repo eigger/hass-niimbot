0205ae53356800914ddd8f1fcba3ce12e782e3cb
eigger <eigger87@gmail.com>
2024-09-06 18:57:15 +0900

    Update parser.py

--- custom_components/niimbot/niimprint/parser.py
+++ custom_components/niimbot/niimprint/parser.py
@@ -64,7 +64,7 @@ class NiimbotDevice:
                     device.sw_version = str(await printer.get_info(InfoEnum.SOFTVERSION))
                 if not device.devicetype:
                     device.devicetype = await printer.get_info(InfoEnum.DEVICETYPE)
-                    device.model = get_printer_meta_by_id(device.devicetype)["model"].name if not device.devicetype else "Unknown"
+                    device.model = get_printer_meta_by_id(device.devicetype)["model"].name if not device.devicetype else str(device.devicetype)
 
                 # if not device.density:
                 #     device.density = str(await printer.get_info(InfoEnum.DENSITY))
