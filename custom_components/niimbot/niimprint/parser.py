248f58c82e21b9a6ee9c0a6e3f5285caab7dad54
eigger <eigger87@gmail.com>
2024-09-06 19:39:05 +0900

    Update parser.py

--- custom_components/niimbot/niimprint/parser.py
+++ custom_components/niimbot/niimprint/parser.py
@@ -1,6 +1,7 @@
 """Parser for Niimbot BLE devices"""
 import dataclasses
 import logging
+import asyncio
 
 # from logging import Logger
 from PIL import Image, ImageOps
@@ -41,75 +42,78 @@ class NiimbotDevice:
     def __init__(self, address, continuous_connection):
         self.address = address
         self.continuous_connection = continuous_connection
+        self.lock = asyncio.Lock()
         super().__init__()
 
     async def update_device(self, ble_device: BLEDevice) -> BLEData:
         """Connects to the device through BLE and retrieves relevant data"""
-        device = BLEData()
-        device.name = ble_device.name
-        device.address = ble_device.address
-    
-        try:
-            client = BleakClient(ble_device)
-            if not client.is_connected:
-                client = await establish_connection(BleakClient, ble_device, ble_device.address)
-            if client.is_connected:
-                printer = PrinterClient(client)
-                await printer.start_notify()
-                if not device.serial_number:
-                    device.serial_number = str(await printer.get_info(InfoEnum.DEVICESERIAL))
-                if not device.hw_version:
-                    device.hw_version = str(await printer.get_info(InfoEnum.HARDVERSION))
-                if not device.sw_version:
-                    device.sw_version = str(await printer.get_info(InfoEnum.SOFTVERSION))
-                if not device.devicetype:
-                    device.devicetype = await printer.get_info(InfoEnum.DEVICETYPE)
-                    meta = get_printer_meta_by_id(int(device.devicetype))
-                    device.model = meta["model"].name if meta else str(device.devicetype)
+        async with self.lock: 
+            device = BLEData()
+            device.name = ble_device.name
+            device.address = ble_device.address
+        
+            try:
+                client = BleakClient(ble_device)
+                if not client.is_connected:
+                    client = await establish_connection(BleakClient, ble_device, ble_device.address)
+                if client.is_connected:
+                    printer = PrinterClient(client)
+                    await printer.start_notify()
+                    if not device.serial_number:
+                        device.serial_number = str(await printer.get_info(InfoEnum.DEVICESERIAL))
+                    if not device.hw_version:
+                        device.hw_version = str(await printer.get_info(InfoEnum.HARDVERSION))
+                    if not device.sw_version:
+                        device.sw_version = str(await printer.get_info(InfoEnum.SOFTVERSION))
+                    if not device.devicetype:
+                        device.devicetype = await printer.get_info(InfoEnum.DEVICETYPE)
+                        meta = get_printer_meta_by_id(int(device.devicetype))
+                        device.model = meta["model"].name if meta else str(device.devicetype)
 
-                # if not device.density:
-                #     device.density = str(await printer.get_info(InfoEnum.DENSITY))
-                # if not device.printspeed:
-                #     device.printspeed = str(await printer.get_info(InfoEnum.PRINTSPEED))
-                # if not device.labeltype:
-                #     device.labeltype = str(await printer.get_info(InfoEnum.LABELTYPE))
-                # if not device.languagetype:
-                #     device.languagetype = str(await printer.get_info(InfoEnum.LANGUAGETYPE))
-                # if not device.autoshutdowntime:
-                #     device.autoshutdowntime = str(await printer.get_info(InfoEnum.AUTOSHUTDOWNTIME))
+                    # if not device.density:
+                    #     device.density = str(await printer.get_info(InfoEnum.DENSITY))
+                    # if not device.printspeed:
+                    #     device.printspeed = str(await printer.get_info(InfoEnum.PRINTSPEED))
+                    # if not device.labeltype:
+                    #     device.labeltype = str(await printer.get_info(InfoEnum.LABELTYPE))
+                    # if not device.languagetype:
+                    #     device.languagetype = str(await printer.get_info(InfoEnum.LANGUAGETYPE))
+                    # if not device.autoshutdowntime:
+                    #     device.autoshutdowntime = str(await printer.get_info(InfoEnum.AUTOSHUTDOWNTIME))
 
 
-                device.sensors['density'] = device.density
-                device.sensors['printspeed'] = device.printspeed
-                device.sensors['labeltype'] = device.labeltype
-                device.sensors['languagetype'] = device.languagetype
-                device.sensors['autoshutdowntime'] = device.autoshutdowntime
+                    device.sensors['density'] = device.density
+                    device.sensors['printspeed'] = device.printspeed
+                    device.sensors['labeltype'] = device.labeltype
+                    device.sensors['languagetype'] = device.languagetype
+                    device.sensors['autoshutdowntime'] = device.autoshutdowntime
 
-                heartbeat = await printer.heartbeat()
-                device.sensors['closingstate'] =  heartbeat["closingstate"]
-                device.sensors['powerlevel'] =  heartbeat["powerlevel"]
-                device.sensors['paperstate'] =  heartbeat["paperstate"]
-                device.sensors['rfidreadstate'] =  heartbeat["rfidreadstate"]
-                device.sensors['battery'] = float(heartbeat["powerlevel"]) * 25.0
-                await printer.stop_notify()
-                if not self.continuous_connection:
-                    await client.disconnect()
-        except:
-            await client.disconnect()
+                    heartbeat = await printer.heartbeat()
+                    device.sensors['closingstate'] =  heartbeat["closingstate"]
+                    device.sensors['powerlevel'] =  heartbeat["powerlevel"]
+                    device.sensors['paperstate'] =  heartbeat["paperstate"]
+                    device.sensors['rfidreadstate'] =  heartbeat["rfidreadstate"]
+                    device.sensors['battery'] = float(heartbeat["powerlevel"]) * 25.0
+                    await printer.stop_notify()
+                    if not self.continuous_connection:
+                        await client.disconnect()
+            except:
+                await client.disconnect()
 
-        return device
+            return device
     
     async def print_image(self, ble_device: BLEDevice, image: Image):
-        try:
-            client = BleakClient(ble_device)
-            if not client.is_connected:
-                client = await establish_connection(BleakClient, ble_device, ble_device.address)
-            if client.is_connected:
-                printer = PrinterClient(client)
-                await printer.start_notify()
-                await printer.print_image(image)
-                await printer.stop_notify()
-                if not self.continuous_connection:
-                    await client.disconnect()
-        except:
-            await client.disconnect()
+        async with self.lock: 
+            try:
+                client = BleakClient(ble_device)
+                if not client.is_connected:
+                    client = await establish_connection(BleakClient, ble_device, ble_device.address)
+                if client.is_connected:
+                    printer = PrinterClient(client)
+                    await printer.start_notify()
+                    await printer.print_image(image)
+                    await printer.stop_notify()
+                    if not self.continuous_connection:
+                        await client.disconnect()
+            except:
+                await client.disconnect()
