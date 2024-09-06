038509fe898d8332ea8a7dfd3398dce307d17017
eigger <eigger87@gmail.com>
2024-09-06 18:50:23 +0900

    Update

--- custom_components/niimbot/niimprint/parser.py
+++ custom_components/niimbot/niimprint/parser.py
@@ -22,7 +22,7 @@ class BLEData:
     name: str = ""
     identifier: str = ""
     address: str = ""
-    model: str = ""
+    model: str = None
     serial_number: str = None
     density: str = None
     printspeed: str = None
@@ -49,52 +49,52 @@ class NiimbotDevice:
         device.name = ble_device.name
         device.address = ble_device.address
     
-        # try:
-        client = BleakClient(ble_device)
-        if not client.is_connected:
-            client = await establish_connection(BleakClient, ble_device, ble_device.address)
-        if client.is_connected:
-            printer = PrinterClient(client)
-            await printer.start_notify()
-            if not device.serial_number:
-                device.serial_number = str(await printer.get_info(InfoEnum.DEVICESERIAL))
-            if not device.hw_version:
-                device.hw_version = str(await printer.get_info(InfoEnum.HARDVERSION))
-            if not device.sw_version:
-                device.sw_version = str(await printer.get_info(InfoEnum.SOFTVERSION))
-            if not device.devicetype:
-                device.devicetype = await printer.get_info(InfoEnum.DEVICETYPE)
-                #device.model = get_printer_meta_by_id(device.devicetype)["model"].name
+        try:
+            client = BleakClient(ble_device)
+            if not client.is_connected:
+                client = await establish_connection(BleakClient, ble_device, ble_device.address)
+            if client.is_connected:
+                printer = PrinterClient(client)
+                await printer.start_notify()
+                if not device.serial_number:
+                    device.serial_number = str(await printer.get_info(InfoEnum.DEVICESERIAL))
+                if not device.hw_version:
+                    device.hw_version = str(await printer.get_info(InfoEnum.HARDVERSION))
+                if not device.sw_version:
+                    device.sw_version = str(await printer.get_info(InfoEnum.SOFTVERSION))
+                if not device.devicetype:
+                    device.devicetype = await printer.get_info(InfoEnum.DEVICETYPE)
+                    device.model = get_printer_meta_by_id(device.devicetype)["model"].name if not device.devicetype else "Unknown"
 
-            # if not device.density:
-            #     device.density = str(await printer.get_info(InfoEnum.DENSITY))
-            # if not device.printspeed:
-            #     device.printspeed = str(await printer.get_info(InfoEnum.PRINTSPEED))
-            # if not device.labeltype:
-            #     device.labeltype = str(await printer.get_info(InfoEnum.LABELTYPE))
-            # if not device.languagetype:
-            #     device.languagetype = str(await printer.get_info(InfoEnum.LANGUAGETYPE))
-            # if not device.autoshutdowntime:
-            #     device.autoshutdowntime = str(await printer.get_info(InfoEnum.AUTOSHUTDOWNTIME))
+                # if not device.density:
+                #     device.density = str(await printer.get_info(InfoEnum.DENSITY))
+                # if not device.printspeed:
+                #     device.printspeed = str(await printer.get_info(InfoEnum.PRINTSPEED))
+                # if not device.labeltype:
+                #     device.labeltype = str(await printer.get_info(InfoEnum.LABELTYPE))
+                # if not device.languagetype:
+                #     device.languagetype = str(await printer.get_info(InfoEnum.LANGUAGETYPE))
+                # if not device.autoshutdowntime:
+                #     device.autoshutdowntime = str(await printer.get_info(InfoEnum.AUTOSHUTDOWNTIME))
 
 
-            device.sensors['density'] = device.density
-            device.sensors['printspeed'] = device.printspeed
-            device.sensors['labeltype'] = device.labeltype
-            device.sensors['languagetype'] = device.languagetype
-            device.sensors['autoshutdowntime'] = device.autoshutdowntime
+                device.sensors['density'] = device.density
+                device.sensors['printspeed'] = device.printspeed
+                device.sensors['labeltype'] = device.labeltype
+                device.sensors['languagetype'] = device.languagetype
+                device.sensors['autoshutdowntime'] = device.autoshutdowntime
 
-            heartbeat = await printer.heartbeat()
-            device.sensors['closingstate'] =  heartbeat["closingstate"]
-            device.sensors['powerlevel'] =  heartbeat["powerlevel"]
-            device.sensors['paperstate'] =  heartbeat["paperstate"]
-            device.sensors['rfidreadstate'] =  heartbeat["rfidreadstate"]
-            device.sensors['battery'] = float(heartbeat["powerlevel"]) * 25.0
-            await printer.stop_notify()
-            if not self.continuous_connection:
-                await client.disconnect()
-        # except:
-        #     await client.disconnect()
+                heartbeat = await printer.heartbeat()
+                device.sensors['closingstate'] =  heartbeat["closingstate"]
+                device.sensors['powerlevel'] =  heartbeat["powerlevel"]
+                device.sensors['paperstate'] =  heartbeat["paperstate"]
+                device.sensors['rfidreadstate'] =  heartbeat["rfidreadstate"]
+                device.sensors['battery'] = float(heartbeat["powerlevel"]) * 25.0
+                await printer.stop_notify()
+                if not self.continuous_connection:
+                    await client.disconnect()
+        except:
+            await client.disconnect()
 
         return device
     
